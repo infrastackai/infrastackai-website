11643d1a59cef3edccf39e7bfe7d40820f535f93
Aykut Gedik <aykutged@gmail.com>
2025-01-03 10:29:08 -0800

    added favicon and meta

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,12 @@
 import type { ReactNode } from 'react';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
-
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'InfraStack AI | Your AI-Ops Agents',
+  description: 'Supercharge Infrastructure, SRE, and DevOps with AI Agents. Transform repetitive and time-consuming tasks into autonomous workflows. Save time, reduce errors, and improve productivity.',
+}
 
 export default function Layout({
   children,
