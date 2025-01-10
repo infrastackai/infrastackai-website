44d810681152918c9416c349360c32958ca3336c
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2025-01-11 02:44:02 +0300

    fix docs header

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,11 +1,14 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
+import "./docs.css"
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DocsLayout nav={{
+      enabled: true,
+    }} tree={source.pageTree} {...baseOptions}>
       {children}
     </DocsLayout>
   );
