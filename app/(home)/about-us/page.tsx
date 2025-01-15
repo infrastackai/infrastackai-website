b9f8e28038af9b4f86e6c1fe548b2a4d549dec76
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2025-01-15 13:18:17 +0300

    about us responsivity

--- app/(home)/about-us/page.tsx
+++ app/(home)/about-us/page.tsx
@@ -28,25 +28,25 @@ const people = [
 
 export default function AboutUs() {
   return (
-    <div className="w-full flex flex-col items-center justify-center my-4">
-      <div className="flex flex-col gap-2 items-stretch w-3/4 justify-center min-h-screen relative w-full mt-[104px] overflow-hidden space-y-4">
+    <div className="container w-full flex flex-col items-center justify-center my-12">
+      <div className="flex flex-col gap-12 items-stretch px-4 md:px-0 md:w-3/4 justify-center min-h-screen relative mt-[104px] overflow-hidden space-y-4">
         <div className="flex-col items-start justify-start">
-          <h1 className="text-7xl font-bold font-semibold trackin text-5xl bg-gradient-to-b from-black via-black via-[29%] to-neutral-500 bg-clip-text text-transparent dark:from-white dark:via-white dark:to-neutral-500">
+          <h1 className="text-4xl md:text-7xl font-bold bg-gradient-to-b from-black via-black via-[29%] to-neutral-500 bg-clip-text text-transparent dark:from-white dark:via-white dark:to-neutral-500">
             About Us.
           </h1>
-          <p className="text-muted-foreground mt-2 text-lg font-[family-name:var(--font-geist-mono)]">
+          <p className="text-muted-foreground mt-2 text-base md:text-lg font-[family-name:var(--font-geist-mono)]">
             Learn more about us.
           </p>
         </div>
-        <div className="grid grid-cols-1 gap-6">
+        <div className="grid grid-cols-1 gap-4 md:gap-6">
           <Card className="glow-hover mb-2 md:mb-4">
             <CardHeader>
-              <h2 className="text-3xl font-bold font-semibold trackin text-xl bg-gradient-to-b from-black via-black via-[29%] to-neutral-500 bg-clip-text text-transparent dark:from-white dark:via-white dark:to-neutral-500">
+              <h2 className="text-2xl md:text-3xl font-bold bg-gradient-to-b from-black via-black via-[29%] to-neutral-500 bg-clip-text text-transparent dark:from-white dark:via-white dark:to-neutral-500">
                 Our Mission
               </h2>
             </CardHeader>
             <CardContent>
-              <p className="text-muted-foreground mt-2 text-lg font-[family-name:var(--font-geist-mono)]">
+              <p className="text-muted-foreground mt-2 text-base md:text-lg font-[family-name:var(--font-geist-mono)]">
                 To empower businesses by delivering innovative technology
                 solutions that drive growth, efficiency, and success. At the
                 heart of our mission lies a commitment to helping organizations
@@ -63,12 +63,12 @@ export default function AboutUs() {
 
           <Card className="glow-hover mb-2 md:mb-4">
             <CardHeader>
-              <h2 className="text-3xl font-bold font-semibold trackin text-xl bg-gradient-to-b from-black via-black via-[29%] to-neutral-500 bg-clip-text text-transparent dark:from-white dark:via-white dark:to-neutral-500">
+              <h2 className="text-2xl md:text-3xl font-bold bg-gradient-to-b from-black via-black via-[29%] to-neutral-500 bg-clip-text text-transparent dark:from-white dark:via-white dark:to-neutral-500">
                 Our Vision
               </h2>
             </CardHeader>
             <CardContent>
-              <p className="text-muted-foreground mt-2 text-lg font-[family-name:var(--font-geist-mono)]">
+              <p className="text-muted-foreground mt-2 text-base md:text-lg font-[family-name:var(--font-geist-mono)]">
                 To become a global leader in delivering cutting-edge services
                 that redefine industry standards while fostering long-lasting
                 client relationships built on trust, transparency, and mutual
@@ -86,8 +86,10 @@ export default function AboutUs() {
         </div>
 
         <div className="mt-8">
-          <h2 className="text-5xl font-bold font-semibold trackin text-3xl bg-gradient-to-b from-black via-black via-[29%] to-neutral-500 bg-clip-text text-transparent dark:from-white dark:via-white dark:to-neutral-500 my-4">Meet Our Team</h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <h2 className="text-3xl md:text-5xl font-bold bg-gradient-to-b from-black via-black via-[29%] to-neutral-500 bg-clip-text text-transparent dark:from-white dark:via-white dark:to-neutral-500 my-4">
+            Meet Our Team
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
             {people.map((person) => (
               <Card key={person.name}>
                 <CardHeader>
@@ -100,25 +102,29 @@ export default function AboutUs() {
                       alt={person.name}
                     />
                     <div className="pointer-events-none flex flex-col gap-1">
-                      <p className="[&_b]:md:font-semibold [&_strong]:md:font-semibold font-mono text-base/[1.125rem] md:text-lg/[1.5rem] text-gray-300">
+                      <p className="font-mono text-sm md:text-base text-gray-300">
                         {person.name}
                       </p>
-                      <p className="[&_b]:md:font-semibold [&_strong]:md:font-semibold font-mono text-sm/[1.125rem] md:text-base/[1.375rem] text-neutral-600 dark:text-neutral-400">
+                      <p className="font-mono text-xs md:text-sm text-neutral-600 dark:text-neutral-400">
                         {person.role}
                       </p>
                     </div>
                   </div>
                 </CardHeader>
                 <CardContent>
-                  <p className="text-muted-foreground mt-2 text-lg font-[family-name:var(--font-geist-mono)]">{person.description}</p>
+                  <p className="text-muted-foreground mt-2 text-base md:text-lg font-[family-name:var(--font-geist-mono)]">
+                    {person.description}
+                  </p>
                 </CardContent>
               </Card>
             ))}
           </div>
         </div>
 
-        <div className="text-center mt-8 text-muted-foreground mt-2 text-lg font-[family-name:var(--font-geist-mono)]">
-          <Button>CONTACT US</Button>
+        <div className="text-center mt-8">
+          <Button className="text-base md:text-lg font-[family-name:var(--font-geist-mono)]">
+            CONTACT US
+          </Button>
         </div>
       </div>
     </div>
