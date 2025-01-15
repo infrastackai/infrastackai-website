fc3f72bf1376e01858a0c1bda7b31a16855b1966
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2025-01-15 12:19:21 +0300

    fixing blog toc height (#9)
    * fixing blog toc height
    
    * refactor

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -4,13 +4,15 @@ import type { ReactNode } from "react";
 import { blog as source } from "@/lib/source";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import "./blog.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Header />
       <div className="mt-24">
-        <DocsLayout tree={source.pageTree} sidebar={{ enabled: false }}>
+        <DocsLayout
+          nav={{ component: <Header /> }}
+          tree={source.pageTree} sidebar={{ enabled: false }}>
           {children}
         </DocsLayout>
       </div>
