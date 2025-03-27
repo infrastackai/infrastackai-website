3e3eb0c7c9de297285861bc8edf3f40c386d1582
Aykut Gedik <aykutged@gmail.com>
2025-03-27 20:40:35 +0300

    margin on mobile

--- app/(home)/request-early-access/page.tsx
+++ app/(home)/request-early-access/page.tsx
@@ -2,7 +2,7 @@ import Client from "./client";
 
 export default function Page() {
     return (
-        <div className="flex-grow mt-[120px] mb-[60px] flex flex-col items-center space-y-4">
+        <div className="flex-grow mt-[120px] mb-[60px] ml-4 mr-4 md:ml-0 md:mr-0 flex flex-col items-center space-y-4">
             <Client />
         </div>
     )
